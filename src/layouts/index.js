48d384c44bae55a40a60f1a43d23e6928dbed230
Saito Nakamura <saitonakamuramb@gmail.com>
2017-10-25 00:47:41 +0300

    jsx brackets same line and traling comma all

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -10,23 +10,20 @@ const Header = () => (
     style={{
       background: 'rebeccapurple',
       marginBottom: '1.45rem',
-    }}
-  >
+    }}>
     <div
       style={{
         margin: '0 auto',
         maxWidth: 960,
         padding: '1.45rem 1.0875rem',
-      }}
-    >
+      }}>
       <h1 style={{margin: 0}}>
         <Link
           to="/"
           style={{
             color: 'white',
             textDecoration: 'none',
-          }}
-        >
+          }}>
           Gatsby
         </Link>
       </h1>
@@ -44,8 +41,7 @@ const TemplateWrapper = ({children}) => (
         maxWidth: 960,
         padding: '0px 1.0875rem 1.45rem',
         paddingTop: 0,
-      }}
-    >
+      }}>
       {children()}
     </div>
   </div>
