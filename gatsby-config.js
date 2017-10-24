e3f1d17eb93ee4a4d09ed35c3f7ae0c5ff4edc37
Alexander Kurganow <me@akurganow.ru>
2017-10-24 23:26:00 +0300

    Add contentful plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,14 @@
+const keys = require('./keys')
+
 module.exports = {
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: keys.contentful.spaceId,
+        accessToken: keys.contentful.token,
+      },
+    },
+  ],
 }
