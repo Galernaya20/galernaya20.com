ae3c4332e3f130182d0f1d2f5253a907a80aeea8
Saito Nakamura <saitonakamuramb@gmail.com>
2017-10-25 02:58:44 +0300

    base styles

--- src/components/LiveRecording/LiveRecording.js
+++ src/components/LiveRecording/LiveRecording.js
@@ -3,6 +3,11 @@
 import React from 'react'
 import st from './LiveRecording.module.css'
 
+const videoSize = {
+  width: 400,
+  height: 250,
+}
+
 const LiveRecording = () => (
   <div className={st.container}>
     <h2>Запись live</h2>
@@ -10,9 +15,27 @@ const LiveRecording = () => (
       Мы можем записать как вы записываетесь <br />И прочие интересные моменты этой части
     </p>
     <p>Стоимость от 600 руб.</p>
-    <div>
-      <span>Хоба! Видео 1</span>
-      <span>Хоба! Видео 2</span>
+    <div className={st.videoContainer}>
+      <div>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/FUtrw7GtdfM"
+          frameBorder="0"
+          allowFullscreen
+          {...videoSize}
+        />
+      </div>
+      <div>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/DcJFdCmN98s"
+          frameBorder="0"
+          allowFullscreen
+          {...videoSize}
+        />
+      </div>
     </div>
   </div>
 )
