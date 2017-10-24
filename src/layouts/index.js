15ef0cea7df699f0d8552fd45fb6decd89dfce8d
a.korotaev2 <a.korotaev2@tinkoff.ru>
2017-10-24 21:43:42 +0300

    add helmet headers and metadata

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -62,3 +62,47 @@ TemplateWrapper.propTypes = {
 }
 
 export default TemplateWrapper
+
+export const defaultHelmetMeta = [
+  {
+    name: 'theme-color',
+    content: '#ffffff',
+  },
+  {
+    property: 'og:title',
+    content: 'SPB Frontend',
+  },
+  {
+    property: 'og:description',
+    content: 'Петербургское сообщество фронтенд-разработчиков',
+  },
+  {
+    property: 'og:image',
+    content:
+      'https://ucarecdn.com/3d33c161-b440-4710-b3dd-ad980640dba8/-/preview/-/resize/1200x/-/crop/1200x630/0,300/spb_frontend_social.png',
+  },
+  {
+    property: 'og:url',
+    content: 'http://spb-frontend.ru/',
+  },
+  {
+    name: 'twitter:card',
+    content: 'summary_large_image',
+  },
+  {
+    name: 'twitter:site',
+    content: '@spb_frontend',
+  },
+  {
+    name: 'twitter:image:alt',
+    content: 'SPB Frontend. Frontend artisans',
+  },
+  {
+    name: 'description',
+    content: 'Петербургское сообщество фронтенд-разработчиков',
+  },
+  {
+    name: 'keywords',
+    content: 'frontend, spb-frontend',
+  },
+]
