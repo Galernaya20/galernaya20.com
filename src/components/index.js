456fc706e0b063952c3a7345eb839e944900c84d
Zarema Khalilova <zarema.khalilova@gmail.com>
2017-11-01 22:38:19 +0300

    Update Content, remove Summary

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,4 @@
 export {Content} from './Content/Content'
 export {Header} from './Header/Header'
 export {Presentation} from './Presentation/Presentation'
-export {Summary} from './Summary/Summary'
 export {Reviews} from './Reviews/Reviews'
--- src/pages/studio/a.js
+++ src/pages/studio/a.js
@@ -1,25 +1,25 @@
 //@flow
 
 import React from 'react'
-import {Header, Presentation, Summary} from '../../components'
+import {Header, Presentation, Content} from '../../components'
 
 const StudioAPage = () => (
   <div>
     <Header title="Аренда студия звукозаписи А" description="Небольшая студия для записи вокала" />
-    <Summary secondary={<Presentation background={{image: 'http://via.placeholder.com/600x600'}} />}>
+    <Content presentation={<Presentation background={{image: 'http://via.placeholder.com/600x600'}} />}>
       <h2>Оснащение студии</h2>
       <p>
         Студия А состоит из просторной контрольной комнаты и большого тон-зала, общая площадь студии составляет 56
         квадратных метров.
       </p>
-    </Summary>
-    <Summary>
+    </Content>
+    <Content>
       <h2>Стоимость аренды</h2>
       <p>Прайс...</p>
-    </Summary>
-    <Summary>
+    </Content>
+    <Content>
       <h2>Календарь</h2>
-    </Summary>
+    </Content>
   </div>
 )
 
