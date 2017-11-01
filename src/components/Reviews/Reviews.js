6c8d1bb3735e642c1dde197a430665ccbdc8922a
Alexander Kurganow <me@akurganow.ru>
2017-11-01 23:24:50 +0300

    Style Reviews

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -3,6 +3,7 @@
 import React, {Component} from 'react'
 import cn from 'classnames'
 import st from './Reviews.module.css'
+import {Content} from '../Content/Content'
 
 type PropsT = {
   reviews: Array<{author: string, text: string, photo: string}>,
@@ -12,13 +13,19 @@ class Reviews extends Component<PropsT> {
   render() {
     const {reviews} = this.props
     return (
-      <div className={st.reviews}>
+      <Content className={st.reviews}>
         {reviews.map(review => (
           <div key={review.author} className={st.review}>
-            <span className={st.author}>{review.author}</span>
+            <picture className={st.autor_picture}>
+              <img src={review.photo} width="160" />
+            </picture>
+            <div className={st.content}>
+              <p className={st.text}>&ldquo;{review.text}&rdquo;</p>
+              <span className={st.author}>{review.author}</span>
+            </div>
           </div>
         ))}
-      </div>
+      </Content>
     )
   }
 }
--- src/components/Reviews/Reviews.stories.js
+++ src/components/Reviews/Reviews.stories.js
@@ -9,7 +9,7 @@ storiesOf('Reviews', module)
       reviews={[
         {
           text:
-            'Suspendisse suscipit, tortor sed tincidunt molestie, justo arcu consequat lorem, non mattis lorem magna eu odio. Proin vulputate volutpat ultrices. Donec et placerat diam. Sed feugiat, augue id varius bibendum, dui tortor ullamcorper orci, vel pulvinar nisl mauris quis ligula. Sed ultricies enim nec lacus tincidunt, quis sollicitudin ipsum pharetra. Proin vitae mollis tortor. Donec non mi vitae neque feugiat semper vitae quis odio. Donec semper ex quis massa porttitor, eget condimentum leo ullamcorper. Suspendisse potenti. Proin at purus elementum, tristique quam non, pharetra tortor. Cras scelerisque sem nisl, eget finibus dolor tincidunt a.',
+            'Suspendisse suscipit, tortor sed tincidunt molestie, justo arcu consequat lorem, non mattis lorem magna eu odio.',
           author: 'Ivan Tooloop',
           photo: 'https://ucarecdn.com/5b802641-cd9d-4a10-adf1-3dffa9709e29/akurganowsquare.jpg',
         },
@@ -21,19 +21,19 @@ storiesOf('Reviews', module)
       reviews={[
         {
           text:
-            'Suspendisse suscipit, tortor sed tincidunt molestie, justo arcu consequat lorem, non mattis lorem magna eu odio. Proin vulputate volutpat ultrices. Donec et placerat diam. Sed feugiat, augue id varius bibendum, dui tortor ullamcorper orci, vel pulvinar nisl mauris quis ligula. Sed ultricies enim nec lacus tincidunt, quis sollicitudin ipsum pharetra. Proin vitae mollis tortor. Donec non mi vitae neque feugiat semper vitae quis odio. Donec semper ex quis massa porttitor, eget condimentum leo ullamcorper. Suspendisse potenti. Proin at purus elementum, tristique quam non, pharetra tortor. Cras scelerisque sem nisl, eget finibus dolor tincidunt a.',
+            'Suspendisse suscipit, tortor sed tincidunt molestie, justo arcu consequat lorem, non mattis lorem magna eu odio. Proin vulputate volutpat ultrices.',
           author: 'Ivan Tooloop',
           photo: 'https://ucarecdn.com/5b802641-cd9d-4a10-adf1-3dffa9709e29/akurganowsquare.jpg',
         },
         {
           text:
-            'Vestibulum erat ex, dictum et dapibus sed, tempor sit amet mi. Sed rutrum enim eget feugiat posuere. Donec at sodales erat. Quisque ultricies posuere tortor, non laoreet metus dignissim vel. Suspendisse placerat consequat metus, dignissim vehicula velit. Integer vulputate nisl a tellus rhoncus, et rutrum justo auctor. Ut neque nisi, ultrices sit amet imperdiet nec, fermentum eget odio. Nullam mi dolor, cursus eget tellus ac, volutpat auctor urna. Morbi condimentum turpis nec massa aliquam, at eleifend nunc hendrerit. Ut sit amet ante mi. Phasellus vitae eros sagittis, dictum metus ut, molestie ante. Proin elementum molestie arcu ut hendrerit. Donec auctor, leo id imperdiet pulvinar, elit purus viverra libero, sed hendrerit arcu erat id odio. Vestibulum efficitur justo eu felis finibus volutpat. Mauris et magna tincidunt, viverra nisi non, lacinia massa. Pellentesque sollicitudin luctus scelerisque.',
+            'Vestibulum erat ex, dictum et dapibus sed, tempor sit amet mi. Sed rutrum enim eget feugiat posuere. Donec at sodales erat.',
           author: 'Alex Kurganov',
           photo: 'https://ucarecdn.com/5b802641-cd9d-4a10-adf1-3dffa9709e29/akurganowsquare.jpg',
         },
         {
           text:
-            'Fusce a augue id sapien porta pretium eu vitae est. In vitae cursus nulla. Vivamus semper, lectus at faucibus luctus, elit neque auctor erat, nec varius lacus augue id nunc. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur dignissim eget orci at mattis. Praesent eu est id odio porttitor gravida nec eu dolor. Sed efficitur diam sit amet ipsum ultricies aliquet. Fusce nec tellus vel libero lacinia accumsan et luctus ex.',
+            'Fusce a augue id sapien porta pretium eu vitae est. In vitae cursus nulla. Vivamus semper, lectus at faucibus luctus, elit neque auctor erat, nec varius lacus augue id nunc.',
           author: 'Batman',
           photo: 'https://ucarecdn.com/5b802641-cd9d-4a10-adf1-3dffa9709e29/akurganowsquare.jpg',
         },
