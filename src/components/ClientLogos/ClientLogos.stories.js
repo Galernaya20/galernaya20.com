5a8ba1a70e0735aed1fccd011887e5cc5e6c73b4
a.korotaev2 <a.korotaev2@tinkoff.ru>
2017-11-01 23:10:12 +0300

    added ClientLogos component

---
+++ src/components/ClientLogos/ClientLogos.stories.js
@@ -0,0 +1,6 @@
+//@flow
+import React from 'react'
+import {storiesOf} from '@storybook/react'
+import {ClientLogos} from './ClientLogos.js'
+
+storiesOf('ClientLogos', module).add('ClientLogos', () => <ClientLogos />)
