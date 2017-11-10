ba20094d3d68aaa6faa52ed40afde342d5b57ddd
a.korotaev2 <a.korotaev2@tinkoff.ru>
2017-11-10 20:37:47 +0300

    hamburger menu styles

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,10 +2,41 @@
 import React from 'react'
 import st from './NavBar.module.css'
 import Link from 'gatsby-link'
+import {HamburgerIcon} from '../icons/HamburgerIcon'
 
 export const NavBar = () => (
   <div className={st.header}>
     <nav className={st.wrapper}>
+      <div className={st.menu}>
+        <div className={st.hamburger}>
+          <HamburgerIcon className={st.hamburgerIcon} />
+        </div>
+
+        <ul className={st.menuList}>
+          <li className={st.menuItem}>
+            <Link to="/">Студия</Link>
+          </li>
+          <li className={st.menuItem}>
+            <Link to="/production">Продакшн</Link>
+          </li>
+          <li className={st.menuItem}>
+            <Link to="/equipment">Оборудование</Link>
+          </li>
+          <li className={st.menuItem}>
+            <Link to="/school">Школа</Link>
+          </li>
+          <li className={st.menuItem}>
+            <Link to="/team">Команда</Link>
+          </li>
+          <li className={st.menuItem}>
+            <Link to="/price">Цены</Link>
+          </li>
+          <li className={st.menuItem}>
+            <Link to="/contacts">Контакты</Link>
+          </li>
+        </ul>
+      </div>
+
       <Link to="/" className={st.logo}>
         <img
           src="http://via.placeholder.com/150x50"
@@ -13,30 +44,6 @@ export const NavBar = () => (
           className={st.logoImg}
         />
       </Link>
-
-      <ul className={st.menu}>
-        <li className={st.menuItem}>
-          <Link to="/">Студия</Link>
-        </li>
-        <li className={st.menuItem}>
-          <Link to="/production">Продакшн</Link>
-        </li>
-        <li className={st.menuItem}>
-          <Link to="/equipment">Оборудование</Link>
-        </li>
-        <li className={st.menuItem}>
-          <Link to="/school">Школа</Link>
-        </li>
-        <li className={st.menuItem}>
-          <Link to="/team">Команда</Link>
-        </li>
-        <li className={st.menuItem}>
-          <Link to="/price">Цены</Link>
-        </li>
-        <li className={st.menuItem}>
-          <Link to="/contacts">Контакты</Link>
-        </li>
-      </ul>
 
       <div className={st.contacts}>8 (812) 994 54 97</div>
     </nav>
