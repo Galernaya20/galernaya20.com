f28e718305cca3a18e6d13f70a1eb094ab77af53
Dmitriy Shved <dshved@hotmail.com>
2018-01-23 21:54:31 +0300

    fix: footer and container

---
+++ src/components/Container/Container.js
@@ -0,0 +1,21 @@
+//@flow
+
+import React from 'react'
+import styled from 'styled-components'
+
+const Self = styled.div`
+  width: 100%;
+  height: 100%;
+`
+const SelfInner = styled.div`
+  width: 100%;
+  max-width: 960px;
+  margin: 0 auto;
+  height: 100%;
+`
+
+export const Container = ({children, className, style}: {children: *, style?: Object, className?: string}) => (
+  <Self className={className} style={style}>
+    <SelfInner>{children}</SelfInner>
+  </Self>
+)
