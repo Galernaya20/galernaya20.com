655ebf8c3f363178785b4e079340e5fb34b6d512
Dmitriy Shved <dshved@hotmail.com>
2018-01-27 18:14:07 +0300

    fix: flow

--- src/components/pages/School/fixture.js
+++ src/components/pages/School/fixture.js
@@ -2,28 +2,53 @@
 
 export default {
   header: {
-    title: 'Образовательные программы',
-    description: {description: 'Курсы лекций про саунд, продюссирование и запись музыки'},
+    title: 'Образовательные программы contentful',
+    description: {
+      description: 'Курсы лекций про саунд, продюссирование и запись музыки content',
+    },
   },
   selfProduction: {
-    title: 'Самопродюссирование',
-    description: 'Что-то про особенности записи инструментов. Для кого, для скольки людей и т.д.',
-    price: '364',
-    link: 'https://galernaya20.com/',
-    src: 'https://via.placeholder.com/900x900',
+    title: 'Самопродюссирование contentful',
+    description: {
+      description: 'Что-то про особенности записи инструментов. Для кого, для скольки людей и т.д. contentful',
+    },
+    price: 983,
+    link: 'https://kek.lol',
   },
   recordingAndMixing: {
-    title: 'Запись и сведение',
-    description: 'Что-то про особенности записи инструментов. Для кого, для скольки людей и т.д.',
-    price: '364',
-    link: 'https://galernaya20.com/',
-    src: 'https://via.placeholder.com/900x900',
+    title: 'Запись и сведение contentful',
+    description: {
+      description:
+        'Что-то про особенности записи инструментов. Для кого, для скольки людей и т.д.Что-то про особенности записи инструментов. Для кого, для скольки людей и т.д. contentful',
+    },
+    price: 432,
+    link: 'https://kekspep.lol',
   },
   dj: {
-    title: 'Диджеинг',
-    description: 'Что-то про особенности записи инструментов. Для кого, для скольки людей и т.д.',
-    price: '549',
-    link: 'https://galernaya20.com/',
-    src: 'https://via.placeholder.com/900x900',
+    title: 'Диджеинг contentful',
+    description: {
+      description:
+        'Диджеинг contentfulДиджеинг contentfulДиджеинг contentfulДиджеинг contentfulДиджеинг contentfulДиджеинг contentfulДиджеинг contentfulДиджеинг contentful',
+    },
+    price: 33425,
+    link: 'https://galef.com',
+  },
+  selfProductionMedia: {
+    file: {
+      url:
+        '//images.contentful.com/vx768p4z763o/attachment_post_2388/0a993490d760786e33dd51c6b09f759e/attachment_post_2388',
+    },
+  },
+  recordingAndMixingMedia: {
+    file: {
+      url:
+        '//images.contentful.com/vx768p4z763o/attachment_post_1996/24ae3db6bcbb1369654655d13f709333/attachment_post_1996',
+    },
+  },
+  djMedia: {
+    file: {
+      url:
+        '//images.contentful.com/vx768p4z763o/attachment_post_1765/43b03f51f332d724daa91e5a4ee0544d/attachment_post_1765',
+    },
   },
 }
