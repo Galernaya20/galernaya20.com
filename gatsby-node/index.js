22cdcace037af4712e2adea4ade7fba888832567
Sergey Lapin <lapanoid@gmail.com>
2018-03-07 00:14:23 +0300

    wip: bootstrap missing pages with equipment page

--- gatsby-node/index.js
+++ gatsby-node/index.js
@@ -1,6 +1,13 @@
 //@flow
 
 exports.createPages = async (gatsby /*:any*/) => {
+  await require('./arrangement')(gatsby)
+  await require('./bitmaking')(gatsby)
+  await require('./instruments')(gatsby)
+  await require('./mastering')(gatsby)
+  await require('./mounting')(gatsby)
+  await require('./recordingReduction')(gatsby)
+  await require('./vocal')(gatsby)
   await require('./contacts')(gatsby)
   await require('./training')(gatsby)
   await require('./team')(gatsby)
---
+++ gatsby-node/mastering.js
@@ -0,0 +1,31 @@
+const slash = require('slash')
+const path = require('path')
+
+const Equipment = path.resolve(process.cwd(), 'src/components/pages/Equipment/Equipment.js')
+
+module.exports = async ({graphql, boundActionCreators: {createPage}}) => {
+  const equipmentResult = await graphql(`
+    {
+      contentfulEquipmentPage {
+        header {
+          title
+          description {
+            description
+          }
+        }
+        tabs {
+          title
+          content {
+            content
+          }
+        }
+      }
+    }
+  `)
+
+  createPage({
+    path: '/mastering',
+    component: slash(Equipment),
+    context: equipmentResult.data.contentfulEquipmentPage,
+  })
+}
